7f614b857e9170d78455af35ac85f88ad14981d2
Robbie McElrath <robbiemcelrath@gmail.com>
2013-03-24 21:39:08 -0700

    Added files from old dots repo

---
+++ install.zsh
@@ -0,0 +1,42 @@
+#!/bin/zsh
+#
+# Installs dot files
+#
+
+DOTS=~/.dots
+if [ -d ${DOTS} ]; then
+  echo "Dot file are already installed. Delete your ${DOTS} directory and try again."
+  exit 1
+fi
+
+echo "Cloning dots..."
+hash git >/dev/null && /usr/bin/env git clone git@github.com:robbiemc/dots.git ${DOTS} || {
+  echo "git not installed"
+  exit 1
+}
+
+echo "Switching to dots directory..."
+cd ${DOTS}
+
+echo "Initializing submodules..."
+/usr/bin/env git submodule init && /usr/bin/env git submodule update || {
+  echo "Failed to setup dots submodules"
+  exit 1
+}
+
+echo "Linking rc files..."
+for file in *.dot(N); do
+  rc=".${file%.dot}"
+  if [ -e ${rc} ]; then
+    echo "Backing up ${rc} as ${rc}.bak"
+    mv ~/${rc} ~/${rc}.bak
+  fi
+  ln -s ${DOTS}/${file} ~/${rc}
+done
+
+# Make vim temp directories if they don't exist
+mkdir -p ~/.local/share/vim/swap
+mkdir -p ~/.local/share/vim/undo
+mkdir -p ~/.local/share/vim/backup
+
+echo "Dot files installed! Restart your shell."
---
+++ zsh/aliases.zsh
@@ -0,0 +1,14 @@
+# Removes oh-my-zsh aliases I don't use
+unalias cd..
+unalias cd...
+unalias cd....
+unalias cd.....
+unalias cd/
+
+# Custom aliases
+alias dus='du -sk | sorn -n'
+alias sml='rlwrap sml'
+alias gdb='gdb -silent'
+alias vim='vim -p'
+alias st='git status'
+alias mcm='make clean && make'
